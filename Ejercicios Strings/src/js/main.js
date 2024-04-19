4af09a74a5a439ba2b6009d79e5c59b779bcb937
mspena14 <marlono1naranjo@gmail.com>
2024-04-19 10:28:24 -0500

    Se agregan los archivos al repositorio

---
+++ Ejercicios Strings/src/js/main.js
@@ -0,0 +1,54 @@
+/*     1. Declara dos variables, num1 y num2, asignándoles valores numéricos diferentes y muestra en consola el resultado de sumarlos. */
+let num1 = 5
+let num2 = 4
+
+console.log("la suma es=", num1+num2)
+
+/* 2. Define dos variables de tipo string, simpleString1 y simpleString2, con valores "Hola" y "Mundo" respectivamente.
+Muestra en consola la longitud total de ambas cadenas. */
+/* 3. Crea dos variables de tipo string usando la función String(): stringUsingString1 con el valor "JavaScript" y stringUsingString2 
+con el valor "Ejercicios". */
+/* 4. Utiliza el operador new para crear dos variables de tipo string: stringUsingNewString1 con el valor "Concatenar" y stringUsingNewString2 con 
+el valor "Strings". */
+/* 5. Calcula e imprime en consola la posición del substring "la" en la variable simpleString1. */
+/* 6. Comprueba si la variable simpleString2 contiene el substring "ndo" e imprime el resultado en consola. */
+/* 7. Concatena las variables simpleString1 y simpleString2 utilizando el método concat() y muestra el resultado en consola. */
+/* 8. Concatena las variables simpleString1 y simpleString2 utilizando el operador + y muestra el resultado en consola. */
+/* 9. Utiliza template strings para concatenar las variables simpleString1 y simpleString2 e imprime el resultado en consola. */
+/* 10. Declara una variable stringWithSpaces1 con el valor " TrimStart" y otra variable stringWithSpaces2 con el valor "TrimEnd ".
+Elimina los espacios en blanco al principio y al final de ambas cadenas e imprime el resultado en consola. */
+/* 11. Reemplaza la letra 'o' por la letra 'i' en la variable simpleString1 e imprime el resultado en consola. */
+/* 12. Utiliza el método slice() para obtener los primeros tres caracteres de la variable simpleString2 e imprime el resultado en consola. */
+/* 13. Utiliza el método substring() para obtener una subcadena de la variable simpleString2 que comience en el índice 2 e imprime el resultado en consola. */
+/* 14. Repite la variable simpleString1 dos veces utilizando el método repeat() e imprime el resultado en consola. */
+/* 15. Divide la cadena "Esto es una oración de ejemplo" en un array de palabras y muestra el resultado en consola. */
+/* 16. Convierte la variable simpleString2 a mayúsculas utilizando el método toUpperCase() e imprime el resultado en consola. */
+/* 17. Convierte la variable simpleString1 a minúsculas utilizando el método toLowerCase() e imprime el resultado en consola. */
+/* 18. Declara una variable booleana y asígnale un valor booleano.Muestra en consola su tipo de dato utilizando el operador typeof (). */
+/* 19. Declara una variable arreglo y asígnale un array con al menos tres elementos.Muestra en consola la longitud del array. */
+/* 20. Declara una variable objeto y asígnale un objeto con al menos tres propiedades.Muestra en consola una de las propiedades del objeto. */
+/* 21. Declara una variable nulo y asígnale el valor null.Muestra en consola su tipo de dato utilizando el operador typeof (). */
+/* 22. Declara una variable indefinido y no le asignes ningún valor.Muestra en consola su tipo de dato utilizando el operador typeof (). */
+/* 23. Declara una variable decimal y asígnale un número decimal.Muestra en consola su tipo de dato utilizando el operador typeof (). */
+/* 24. Declara una variable negativo y asígnale un número negativo.Muestra en consola su tipo de dato utilizando el operador typeof (). */
+/* 25. Utiliza el método indexOf() para encontrar la posición del carácter 'a' en la cadena "Casa" e imprime el resultado en consola. */
+/* 26. Comprueba si la cadena "Javascript" incluye el substring "Script" utilizando el método includes() e imprime el resultado en consola. */
+/* 27. Concatena las cadenas "Hola" y "Mundo" utilizando el método concat() y muestra el resultado en consola. */
+/* 28. Concatena las cadenas "Hola" y "Mundo" utilizando el operador + y muestra el resultado en consola. */
+/* 29. Utiliza template strings para concatenar las cadenas "Hola" y "Mundo" e imprime el resultado en consola. */
+/* 30. Declara una variable cadenaConEspacios con el valor " Ejemplo con espacios ".
+Utiliza el método trimStart() y trimEnd() para eliminar los espacios en blanco al principio y al final, respectivamente, e imprime el resultado en consola. */
+/* 31. Reemplaza la palabra "azul" por "rojo" en la cadena "El cielo es azul" utilizando el método replace() e imprime el resultado en consola. */
+/* 32. Utiliza el método slice() para obtener los últimos cuatro caracteres de la cadena "Programación" e imprime el resultado en consola. */
+/* 32. Utiliza el método substring() para obtener una subcadena de la cadena "JavaScript" que comience en el índice 2 e imprime el resultado en consola. */
+/* 34. Repite la cadena "Hola" tres veces utilizando el método repeat() e imprime el resultado en consola. */
+/* 35. Divide la cadena "Esto es una oración de ejemplo" en un array de palabras y muestra el resultado en consola. */
+/* 36. Convierte la cadena "Javascript" a mayúsculas utilizando el método toUpperCase() e imprime el resultado en consola. */
+/* 37. Convierte la cadena "EJEMPLO" a minúsculas utilizando el método toLowerCase() e imprime el resultado en consola. */
+/* 38. Declara una variable numero y asígnale un número.Muestra en consola su tipo de dato utilizando el operador typeof (). */
+/* 39. Declara una variable booleana y asígnale un valor booleano.Muestra en consola su tipo de dato utilizando el operador typeof (). */
+/* 40. Declara una variable arreglo y asígnale un array con al menos tres elementos.Muestra en consola la longitud del array. */
+/* 41. Declara una variable objeto y asígnale un objeto con al menos tres propiedades.Muestra en consola una de las propiedades del objeto. */
+/* 42. Declara una variable nulo y asígnale el valor null.Muestra en consola su tipo de dato utilizando el operador typeof (). */
+/* 43. Declara una variable indefinido y no le asignes ningún valor.Muestra en consola su tipo de dato utilizando el operador typeof (). */
+/* 44. Declara una variable decimal y asígnale un número decimal.Muestra en consola su tipo de dato utilizando el operador typeof (). */
